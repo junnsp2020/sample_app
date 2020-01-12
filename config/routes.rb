473ff35852af63eea636e6c4bf41abfe66431801
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-01-12 20:46:10 +0900

    [Finish] 3-6

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'todolists/new'
 	get "top" => "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    post "todolists"=>"todolists#create"
 
-  post "todolists"=>"todolists#create" 
+    get"todolists"=> "todolists#index"
+
+    get "todaylists/:id"=> "todaylists#show", as:"todolist"
+    #.../todolists/1や.../todolists/3に該当する
 end
 
