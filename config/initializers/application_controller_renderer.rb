d5916ad42fa996f9297548dadf970b8136402599
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-01-13 16:04:29 +0900

    [Start] 3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '15a94b6450a405b1ab4eb754474f0e8411a24b08c432a305d8dac9954f1aa512b85972f74ade535ee2aa4f833e643c9e81a427dea3b410aefa9c408306bab691'
