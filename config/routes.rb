6ac36507693b7cce1ba7088fb8b93fc0cfc55dd0
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-01-13 13:42:44 +0900

    [Finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
     get"todolists"=> "todolists#index"
 
-    get "todaylists/:id"=> "todaylists#show", as:"todolist"
+    get "todolists/:id"=> "todolists#show", as:"todolist"
     #.../todolists/1や.../todolists/3に該当する
     
     get "todolists/:id/edit"=> "todolists#edit", as: "edit_todolist"
+
+    patch "todolists/:id"=>"todolists#update", as:"update_todolist"
 
 end
 
